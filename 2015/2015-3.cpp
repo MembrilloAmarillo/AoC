d8c984715594f7a4397eb838b62538646d7337ff
SadenPain <saschapaezsk8@gmail.com>
2021-02-09 15:47:53 +0000

    Segunda parte del 3er dia

--- 2015/2015-3.cpp
+++ 2015/2015-3.cpp
@@ -1,10 +1,103 @@
-//
-// Created by SadenPain on 23/01/2021.
+
+// Created by_r SadenPain on 23/01/2021.
 //
 #include <iostream>
 #include <string>
 #include <cstdio>
 #include <cassert>
+
+int robo_san (int **coor, FILE *f, int N) {
+
+	for (int x = 0; x < N; x++) {
+        for (int y = 0; y < N; y++) {
+            coor[x][y] = 0;
+        }
+    }
+
+    coor[0][0] = 1;
+    int x_s = 0;  // Creamos coordenadas distintas para ambos
+    int y_s = 0;
+    int x_r = 0;
+    int y_r = 0;
+    int visited_houses = 1;
+
+	int switch_to = 2; // Lo usamos para diferenciar entre
+					   // santa y robot santa
+
+	rewind(f);
+
+	char c;
+
+	while ( (c=getc(f)) != EOF ) {
+		if ( switch_to % 2 == 0 ) {
+			switch(c) {
+				case '^':
+					++y_s;
+				   if (y_s == N)
+					   y_s = 0;
+					break;
+				case 'v':
+					--y_s;
+					if (y_s < 0)
+						y_s = N - 1;
+					break;
+				case '>':
+					++x_s;
+				   if (x_s == N)
+						x_s = 0;
+					break;
+				case '<':
+					--x_s;
+					if (x_s < 0)
+						x_s = N - 1;
+					break;
+				default:
+					break;
+			}
+		}
+		else {
+			switch(c) {
+				case '^':
+					++y_r;
+				   if (y_r == N)
+					   y_r = 0;
+					break;
+				case 'v':
+					--y_r;
+					if (y_r < 0)
+						y_r = N - 1;
+					break;
+				case '>':
+					++x_r;
+				   if (x_r == N)
+						x_r = 0;
+					break;
+				case '<':
+					--x_r;
+					if (x_r < 0)
+						x_r = N - 1;
+					break;
+				default:
+					break;
+			}
+		}
+
+		if (switch_to % 2 == 0) {
+			if (coor[x_s][y_s] == 0) {
+				visited_houses++;
+				coor[x_s][y_s] = 1;
+			}
+		} else {
+			if (coor[x_r][y_r] == 0) {
+				visited_houses++;
+				coor[x_r][y_r] = 1;
+			}
+		}
+		switch_to++;
+	}
+
+	return visited_houses;
+}
 
 int main (int argc, char *argv[])
 {
@@ -39,10 +132,11 @@ int main (int argc, char *argv[])
         coordinates[i] = new int[N];
 
     for (int x_ = 0; x_ < N; x_++) {
-        for (int y_ = 0; y_ < N; y_++) {
-            coordinates[x_][y_] = 0;
+        for (int y_r_ = 0; y_r_ < N; y_r_++) {
+            coordinates[x_][y_r_] = 0;
         }
     }
+
     coordinates[0][0] = 1;
     int x = 0;
     int y = 0;
@@ -74,12 +168,17 @@ int main (int argc, char *argv[])
             default:
                 break;
         }
-        if (coordinates[x][y] == 0)
-            visited_houses++;
-        coordinates[x][y] = 1;
+		if (coordinates[x][y] == 0)
+			visited_houses++;
+		coordinates[x][y] = 1;
     }
 
     std::cout << "Visited houses: " << visited_houses << std::endl;
+	
+	visited_houses = robo_san (coordinates, f, N);
+	
+	std::cout << "Visited houses with robo and santa: " 
+		<< visited_houses << std::endl;
 
     delete[](coordinates);
     fclose(f);
